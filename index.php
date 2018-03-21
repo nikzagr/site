7eff792c312a44fcb4a6d4c7c7df3ed7f1e89e19
nikgzagr <nikgluk@i.ua>
2018-03-21 16:29:20 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+    class Student 
+    {
+
+        public $name , $status ;
+
+        function __construct($name , $status) {
+            $this->name = $name;
+            $this->status = $status;
+            echo $this->name ;
+        }
+        
+        function getStatus () {
+            if ($this->status) {
+                echo ' - присутствует'  ;
+            } else {
+                echo ' - отсутствует' ;
+            }
+        }
+
+        function getName ($name) {
+           echo '<br>'.'Имя студента - '. $this->name ;
+        }
+
+    };
+
+    $student = new Student('Alex', 'absent');
+    $student->getName('');
+    $student->getstatus('');
+    
+    echo '<br>';
+    
+    
+    $student = new Student('Max', '');
+    $student->getStatus('');
+    echo '<br>';
+
+    $student = new Student('Ivan', 'a');
+    $student->getStatus('');
+    $student->getName('');
+
+
+?>
